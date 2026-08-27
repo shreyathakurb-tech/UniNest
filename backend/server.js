18a716551cb9fc11d2b759be77da6588bf0dd8d3
Shreya Thakur <shreyathakurb@gmail.com>
2026-08-27 12:09:48 +0530

    Fix image serving path

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ let latestProperties = [];
 let featuredProperties = [];
 
 const app = express();
-app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/images", express.static(path.join(__dirname, "..", "images")));
 
 app.use(cors());
 app.use(express.json());
