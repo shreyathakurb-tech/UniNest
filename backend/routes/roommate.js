cb512cf8e35e1eb0d36253d447ff6e7578fa80d4
unknown <shreyathakurb@gmail.com>
2026-09-08 20:22:02 +0530

    Update css for roommateAI code

--- backend/routes/roommate.js
+++ backend/routes/roommate.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-const { askRoommateAI } = require("../services/roommateAI");
+const askRoommateAI = require("../services/roommateAI");
 
 router.post("/", async (req, res) => {
 
